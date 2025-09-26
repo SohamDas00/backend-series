8a1b3ee209fef781dad5ab27b3e5b0d016672282
SohamDas00 <jisusoham04@gmail.com>
2025-09-26 08:43:38 +0530

    app.js complete

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express, { json } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("public"))
+app.use(cookieParser());
+
+export {app}
