b568526bc58a6d30cef4e4d9dae1e9c06b22b67e
SohamDas00 <jisusoham04@gmail.com>
2025-09-27 22:20:10 +0530

    added user model

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true,
+        trim:true
+    },
+    fullname:{
+        type: String,
+        required: true,
+        index:true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    watchHistory:{
+        type:Schema.Types.ObjectId,
+        ref:'Video'
+    }
+},{
+    timestamps:true
+});
+
+export const User=mongoose.model("User",userSchema)
